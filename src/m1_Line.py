bbf013f97f5df7c12cc13c32dcd1286b527e2ed6
lugschmo <lugschmo@rose-hulman.edu>
2019-06-26 01:53:44 -0600

    done 11

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -579,7 +579,7 @@ class Line(object):
           :rtype: Point
         """
         # --------------------------------------------------------------
-        # TODO: 11.
+        # DONE: 11.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -587,6 +587,7 @@ class Line(object):
         #        The tests are already written (below).
         #        They include the Example in the above doc-string.
         # --------------------------------------------------------------
+        return self.start.halfway_to(self.end)
 
     def is_parallel(self, line2):
         """
